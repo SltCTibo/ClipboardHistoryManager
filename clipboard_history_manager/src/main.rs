c7ee2462fda38731af15cb4a0c594bd7b5fd200a
Thibault-Barbe <thibault.barbe@epitech.eu>
2024-10-09 01:07:13 +0200

    Adding the first code sample where it only prints whats in the user clipboard

--- clipboard_history_manager/src/main.rs
+++ clipboard_history_manager/src/main.rs
@@ -1,3 +1,9 @@
+use copypasta::{ClipboardContext, ClipboardProvider};
+
 fn main() {
-    println!("Hello, world!");
+    let mut clipboard = ClipboardContext::new().unwrap();
+
+    let clipboard_content = clipboard.get_contents().unwrap_or_else(|_| String::from("The clipboard is empty or there is no access"));
+
+    println!("Actual clipboard content: {}", clipboard_content);
 }
